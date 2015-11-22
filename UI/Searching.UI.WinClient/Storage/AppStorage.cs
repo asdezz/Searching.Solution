cd2d283290283e342d146242e1fda515af6dae38
DENIS <DenKinz@mail.ru>
2015-11-22 11:13:13 +0200

    profile form

---
+++ UI/Searching.UI.WinClient/Storage/AppStorage.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.IO.IsolatedStorage;
+
+namespace Searching.UI.WinClient.Storage
+{
+    public class AppStorage
+    {
+        public void Storage()
+        {
+            using (var appStorage = IsolatedStorageFile.GetUserStoreForApplication())
+            {
+                if (appStorage.FileExists("LS.txt"))
+                {
+                    using (var file = appStorage.OpenFile("LS.txt", FileMode.Open))
+                    {
+                        using (var reader = new StreamReader(file))
+                        {
+                            //counter = int.Parse(reader.ReadLine()); message = reader.ReadLine();
+                        }
+                    }
+                }
+                else
+                {
+                    using (var file = appStorage.OpenFile("LS.txt", FileMode.Create))
+                    {
+                        using (var writer = new StreamWriter(file))
+                        {
+                            writer.WriteLine("0");
+                            writer.WriteLine("No messages..!");
+                        }
+                    }
+                }
+            }
+        }
+
+
+   
+    }
+}
